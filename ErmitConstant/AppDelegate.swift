ef2eaad389bb2361269847e46afe01c8462051c8
Alex Chekodanov <>
2020-10-18 20:58:42 +0300

    Init commit

---
+++ ErmitConstant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ErmitConstant
+//
+//  Created by Alex Chekodanov on 18.10.2020.
+//  Copyright © 2020 Alex Chekodanov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ErmitConstant/Backend/GradientDescent.swift
@@ -0,0 +1,43 @@
+//
+//  GradientDescent.swift
+//  ErmitConstant
+//
+//  Created by Alex Chekodanov on 18.10.2020.
+//  Copyright © 2020 Alex Chekodanov. All rights reserved.
+//
+
+import Foundation
+
+class GradientDescent {
+    static let maxIteration = 10000
+    var matrix: Matrix
+
+    init(matrix: Matrix) {
+        self.matrix = matrix
+    }
+
+    func startSearch(startPoint: ECPoint?) -> ECPoint? {
+        return recFunc(startPoint ?? createFirstPoint())
+    }
+
+    func createFirstPoint() -> ECPoint {
+        return ECPoint()
+    }
+
+    func nextPoint(_ currentPoint: ECPoint) -> ECPoint {
+        return ECPoint()
+    }
+
+    func recFunc(_ point: ECPoint, iteration: Int = 0) -> ECPoint? {
+        guard iteration < GradientDescent.maxIteration else { return nil }
+        if isInPolyhedron(point) {
+            return point
+        } else {
+            return recFunc(nextPoint(point), iteration: iteration + 1)
+        }
+    }
+
+    func isInPolyhedron(_ point: ECPoint) -> Bool {
+        return false
+    }
+}
